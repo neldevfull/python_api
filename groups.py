e0141773099f73535a43dd4d8fe05f6d5b10cdf0
neldevfull <nfrugeri@gmail.com>
2016-07-08 23:43:01 +0000

    Implemented methods

--- groups.py
+++ groups.py
@@ -8,3 +8,23 @@ groups = Blueprint('groups', __name__)
 def list_groups():
     data = jsonify({'message': 'All groups!'})
     return data
+
+@groups.route('/groups/<int:id>', methods=['GET'])
+def get_group(id):
+    data = jsonify({'message': 'Search group %s' % id})
+    return data
+
+@groups.route('/groups', methods=['POST'])
+def insert_groups():
+    data = jsonify({'message': 'Insert group!'})
+    return data
+
+@groups.route('/groups/<int:id>', methods=['PUT'])
+def update_groups(id):
+    data = jsonify({'message': 'Update group %s!' % id})
+    return data
+
+@groups.route('/groups/<int:id>', methods=['DELETE'])
+def delete_groups(id):
+    data = jsonify({'message': 'Delete group %s!' % id})
+    return data
