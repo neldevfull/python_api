99bc8a895b890d3f3713036da3b887ae4d574e4d
neldevfull <nfrugeri@gmail.com>
2016-07-16 17:27:23 +0000

    Refactoring methods POST and GET; implemented method DELETE

--- blueprints/users.py
+++ blueprints/users.py
@@ -5,20 +5,32 @@ from models.models import Users
 import json
 
 users   = Blueprint('users', __name__)
-message = {'message': '', 'data': None}
+message = {}
 
 @users.route('/users', methods=['GET'])
 def get_users():
     users = json.loads(Users.objects.to_json())
     return jsonify({"users": users})
 
-@users.route('/users/<int:id>', methods=['GET'])
+@users.route('/users/<id>', methods=['GET'])
 def get_user(id):
-    data = jsonify({'message': 'Search user %s' % id})
-    return data
+    status = 200
+    user   = Users.objects(id=id)
+
+    if user:
+        message['message'] = 'User successfully found'
+        message['user'] = json.loads(user.to_json())
+    else:
+        message['message'] = 'User not found'
+        message['user'] = None
+        status = 404
+
+    return jsonify(message), status
 
 @users.route('/users', methods=['POST'])
 def insert_users():
+    status = 200
+
     try:
         users = request.get_json()
         user  = Users()
@@ -29,21 +41,40 @@ def insert_users():
         user.save()
 
         message['message'] = 'User successfully registered'
-        message['data']    = json.loads(user.to_json())
+        message['user'] = json.loads(user.to_json())
 
-        return jsonify(message)
     except Exception as e:
         message['message'] = 'Failed to save User'
-        message['data']    = e
+        message['error'] = e
+        status = 500
 
-        return jsonify(message)
+    return jsonify(message), status
 
 @users.route('/users/<int:id>', methods=['PUT'])
 def update_users(id):
     data = jsonify({'message': 'Update User %s!' % id})
     return data
 
-@users.route('/users/<int:id>', methods=['DELETE'])
+@users.route('/users/<id>', methods=['DELETE'])
 def delete_users(id):
-    data = jsonify({'message': 'Delete User %s!' % id})
-    return data
+    status = 200
+
+    try:
+        user = Users.objects(id=id)
+
+        if user:
+            message['message'] = 'User successfully deleted'
+            message['user'] = json.loads(user.to_json())
+
+            user.delete()
+        else:
+            message['message'] = 'User not foud'
+            message['user'] = None
+            status = 404
+
+    except Exception as e:
+        message['message'] = 'Failed to delete User'
+        message['error'] = e
+        status = 500
+
+    return jsonify(message), status
