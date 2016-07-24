81bfcb885641d9818cf6b7cf7de00ba2ebf77988
neldevfull <nfrugeri@gmail.com>
2016-07-24 22:04:13 +0000

    Implemented methods for container

--- api/docker/run.py
+++ api/docker/run.py
@@ -1,27 +1,57 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from docker import Client
-import json
 
-def get_info():
-    try:
-        client = Client(base_url='unix://var/run/docker.sock')
 
-        for cli in client.containers(all=True):
-            network = list(cli.get('NetworkSettings').get('Networks').keys())[0]
+class DockerManager:
+    def __init__(self):
+        try:
+            self.client = Client(base_url='unix://var/run/docker.sock')
+        except Exception as e:
+            print('Fail %s' %e)
 
-            print('Container: %s' %cli.get('Names')[0][1:])
-            print('State: %s' % cli.get('State'))
-            print('Status: %s' % cli.get('Status'))
-            print('Rede: %s' %network)
-            print('IP: %s' %cli.get('NetworkSettings').get('Networks').get(network).get('IPAddress'))
-            print('--------------------------------')
+    def inspect_container(self, container_id):
+        try:
+            print(self.client.inspect_container(container_id))
+        except Exception as e:
+            print('Fail %s' %e)
 
-            # print(cli)
+    def create_container(self, name, hostname, image):
+        try:
+            self.client.create_container(
+                name=name,
+                hostname=hostname,
+                image=image,
+                command='/bin/bash',
+                detach=True,
+                stdin_open=True,
+                tty=True
+            )
+        except Exception as e:
+            print('Fail %s' %e)
 
-    except Exception as e:
-        print('Fail %s'%e)
+    def print_container(self, container):
+        network = list(container.get('NetworkSettings').get('Networks').keys())[0]
+
+        print('Container: %s' %container.get('Names')[0][1:])
+        print('State: %s' % container.get('State'))
+        print('Status: %s' % container.get('Status'))
+        print('Rede: %s' %network)
+        print('IP: %s' %container.get('NetworkSettings').get('Networks').get(network).get('IPAddress'))
+        print('--------------------------------')
+
+    def get_containers(self):
+        try:
+            for cli in self.client.containers(all=True):
+                self.print_container(cli)
+
+        except Exception as e:
+            print('Fail %s' %e)
 
 
 if __name__ == '__main__':
-    get_info()
+    dm = DockerManager()
+
+    # dm.create_container("containerone", "containerone.net", 'ubuntu')
+    # dm.get_info()
+    dm.inspect_container('1f89fdec3803')
